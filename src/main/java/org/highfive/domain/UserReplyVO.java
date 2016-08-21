73c6766304de4cab4694ef0d5fdb6aab7f7284b3
윤주형 <juhyung0818@naver.com>
2016-08-21 17:39:36 +0900

    exception 0.0.3

---
+++ src/main/java/org/highfive/domain/UserReplyVO.java
@@ -0,0 +1,49 @@
+package org.highfive.domain;
+
+import java.util.Date;
+
+public class UserReplyVO {
+
+	private Integer rno;
+	public Integer getRno() {
+		return rno;
+	}
+	public void setRno(Integer rno) {
+		this.rno = rno;
+	}
+	public Integer getBno() {
+		return bno;
+	}
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUphoto() {
+		return uphoto;
+	}
+	public void setUphoto(String uphoto) {
+		this.uphoto = uphoto;
+	}
+	private Integer bno;
+	private String content;
+	private Date regdate;
+	private String uid;
+	private String uphoto;
+}
