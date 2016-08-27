73da4f68f97ac95b5b4ba4d32130f98a9b4b92dd
윤주형 <juhyung0818@naver.com>
2016-08-27 14:59:33 +0900

    pull~!!

--- src/main/java/org/highfive/persistence/highfiveDAO.java
+++ src/main/java/org/highfive/persistence/highfiveDAO.java
@@ -3,15 +3,14 @@ package org.highfive.persistence;
 import java.util.List;
 
 import org.highfive.domain.HighfiveVO;
-import org.highfive.domain.UserVO;
 
-public interface highfiveDAO {
+public interface HighfiveDAO {
 	
-	public void regist(HighfiveVO hf);
-	public void delete(HighfiveVO hf);
-	public int getFlag(HighfiveVO hf) throws Exception;
-	public void accept(HighfiveVO hf) throws Exception;
-	public List<UserVO> highfiveList(String uid) throws Exception; //flag=0
-	public List<UserVO> sendList(String uid) throws Exception; //flag=1
-	public List<UserVO> receiveList(String uid) throws Exception; //flag=2
+	public void regist(HighfiveVO hvo);
+	
+	public HighfiveVO read(Integer hno);
+	
+	public void delete(Integer hno);
+	
+	public List<HighfiveVO> listAll();
 }
